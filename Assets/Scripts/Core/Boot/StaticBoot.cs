7c6a6177cec49673e8845c3b87a710f64539c5f5
Ryan Webber <webber@apple.com>
2021-03-21 18:31:29 -0700

    Using Aseprite now; Added Aseprite importer

--- Assets/Scripts/Core/Boot/StaticBoot.cs
+++ Assets/Scripts/Core/Boot/StaticBoot.cs
@@ -7,6 +7,18 @@ public static class StaticBoot
     private static void Init()
     {
 
+        Debug.Log($@"Booting {Application.productName}@{Application.version}
+    Product Identifier: {Application.identifier}
+    Platform: {Application.platform}
+    Unity Version: {Application.unityVersion}
+    Installer: {Application.installerName}
+    Build GUID: {Application.buildGUID}
+    DataPath: {Application.dataPath}
+    Persistant Data Path: {Application.persistentDataPath}
+    System Language: {Application.systemLanguage}
+"
+        );
+
 #if UNITY_EDITOR
         Debug.Log("Limiting target frame rate to 60...");
         Application.targetFrameRate = 60;
--- Assets/Scripts/Core/Geometry/Pentagon.cs
+++ Assets/Scripts/Core/Geometry/Pentagon.cs
@@ -8,23 +8,31 @@ public struct Pentagon
     public static float CentralAngle = Mathf.PI * 2 / 5;
 
     public Vector2 center;
-    public float radius;
+    public float sideLength;
     public float rotation;
 
     private float theta_0 => Mathf.PI / 2 + rotation;
 
+    public Vector2 Center => center;
     public Vector2 Top => GetVertexAtIndex(0);
     public Vector2 TopRight => GetVertexAtIndex(1);
     public Vector2 BottomRight => GetVertexAtIndex(2);
     public Vector2 BottomLeft => GetVertexAtIndex(3);
     public Vector2 TopLeft => GetVertexAtIndex(4);
 
+    public float Rotation => rotation;
+
+    public float SideLength => sideLength;
+    public float Inradius => 0.1f * Mathf.Sqrt(25 + 10 * Mathf.Sqrt(5)) * SideLength;
+    public float Circumradius => 0.1f * Mathf.Sqrt(50 + 10 * Mathf.Sqrt(5)) * SideLength;
+    public float Height => Inradius + Circumradius;
+
     private Vector2 GetVertexAtIndex(int index)
     {
         float theta = theta_0 - index * CentralAngle;
         float x = Mathf.Cos(theta);
         float y = Mathf.Sin(theta);
-        return new Vector2(x, y) * radius + center;
+        return new Vector2(x, y) * Circumradius + center;
     }
 
     public IEnumerable<Vector2> GetPoints()
