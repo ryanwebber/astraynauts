1832fe93c1793e3de3e4a693631c77277785dbfa
Ryan Webber <webber@apple.com>
2021-05-25 22:44:39 -0700

    Some wall generation happening

--- Assets/Scripts/Core/Environment/World/WorldLoader.cs
+++ Assets/Scripts/Core/Environment/World/WorldLoader.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
-using System;
+using System.Linq;
+using UnityEngine.Assertions;
 
 public class WorldLoader : MonoBehaviour
 {
@@ -16,6 +17,16 @@ public class WorldLoader : MonoBehaviour
     }
 
     [System.Serializable]
+    public class WallSettings
+    {
+        [SerializeField]
+        public Tilemap tilemap;
+
+        [SerializeField]
+        public TileBase tiles;
+    }
+
+    [System.Serializable]
     public class FloorSettings
     {
         [SerializeField]
@@ -26,10 +37,13 @@ public class WorldLoader : MonoBehaviour
     }
 
     [SerializeField]
-    private Vector2Int layoutScale;
+    private int layoutScale;
 
     [SerializeField]
     private FloorSettings floorSettings;
+
+    [SerializeField]
+    private WallSettings wallSettings;
 
     public void LoadWorld(WorldGenerator.WorldLayout layout, System.Action completion)
     {
@@ -39,7 +53,9 @@ public class WorldLoader : MonoBehaviour
     private IEnumerator LoadWorldDistributed(WorldGenerator.WorldLayout layout, System.Action completion)
     {
         var loadFloorsOperation = PlaceFloors(layout);
-        var loader = new SafeLoader(loadFloorsOperation);
+        var loadWallsOperation = PlaceWalls(layout);
+
+        var loader = new SafeLoader(loadFloorsOperation, loadWallsOperation);
 
         foreach (var y in loader.SparseLoad<YieldInstruction>(null))
         {
@@ -47,49 +63,106 @@ public class WorldLoader : MonoBehaviour
         }
     }
 
+    private IEnumerable<IOperation> PlaceWalls(WorldGenerator.WorldLayout layout)
+    {
+        var tileset = wallSettings.tiles;
+        var tilemap = wallSettings.tilemap;
+
+        HashSet<Vector2Int> allFloors = new HashSet<Vector2Int>();
+
+        IEnumerable<IOperation> GenerateWallsNeighboring(Vector2Int cell)
+        {
+            bool IsEmpty(Vector2Int direction) => !allFloors.Contains(cell + direction);
+            IEnumerable<Vector2Int> WalkScaled(Vector2Int from, Vector2Int direction)
+            {
+                for (int i = 0; i < layoutScale; i++)
+                    yield return from + direction * i;
+            }
+
+            if (IsEmpty(Vector2Int.left))
+            {
+                foreach (var position in WalkScaled(from: cell * layoutScale + Vector2Int.left, Vector2Int.up))
+                {
+                    yield return new TileAssignment
+                    {
+                        tile = tileset,
+                        tilemap = tilemap,
+                        position = position
+                    };
+                }
+            }
+
+            if (IsEmpty(Vector2Int.right))
+            {
+                foreach (var position in WalkScaled(from: (cell + Vector2Int.right) * layoutScale, Vector2Int.up))
+                {
+                    yield return new TileAssignment
+                    {
+                        tile = tileset,
+                        tilemap = tilemap,
+                        position = position
+                    };
+                }
+            }
+
+            if (IsEmpty(Vector2Int.down))
+            {
+                foreach (var position in WalkScaled(from: cell * layoutScale + Vector2Int.down, Vector2Int.right))
+                {
+                    yield return new TileAssignment
+                    {
+                        tile = tileset,
+                        tilemap = tilemap,
+                        position = position
+                    };
+                }
+            }
+
+            if (IsEmpty(Vector2Int.up))
+            {
+                foreach (var position in WalkScaled(from: (cell + Vector2Int.up) * layoutScale, Vector2Int.right))
+                {
+                    yield return new TileAssignment
+                    {
+                        tile = tileset,
+                        tilemap = tilemap,
+                        position = position
+                    };
+                }
+            }
+        }
+
+        var floorCells = layout.rooms.AllRooms.SelectMany(r => r.GetAllCells());
+        var hallwayCells = layout.hallways.SelectMany(h => h.Path);
+
+        foreach (var floor in Enumerable.Concat(floorCells, hallwayCells))
+            allFloors.Add(floor);
+
+        return Enumerable.Concat(floorCells, hallwayCells).SelectMany(GenerateWallsNeighboring);
+    }
+
     private IEnumerable<IOperation> PlaceFloors(WorldGenerator.WorldLayout layout)
     {   
         var tileset = floorSettings.tiles.AsCollection();
-        foreach (var room in layout.rooms.AllRooms)
-        {
-            foreach (var cell in room.GetAllCells())
-            {
-                foreach (var position in GetScaled(cell))
-                {
-                    yield return new TileAssignment
-                    {
-                        tile = tileset.NextValue(),
-                        position = position,
-                        tilemap = floorSettings.tilemap
-                    };
-                }
-            }
-        }
 
-        foreach (var hallway in layout.hallways)
-        {
-            foreach (var cell in hallway.Path)
-            {
-                foreach (var position in GetScaled(cell))
-                {
-                    yield return new TileAssignment
-                    {
-                        tile = tileset.NextValue(),
-                        position = position,
-                        tilemap = floorSettings.tilemap
-                    };
-                }
-            }
-        }
+        var floorCells = layout.rooms.AllRooms.SelectMany(r => r.GetAllCells());
+        var hallwayCells = layout.hallways.SelectMany(h => h.Path);
+        var expandedPositions = Enumerable.Concat(floorCells, hallwayCells).SelectMany(GetScaled);
 
-        yield return new LambdaOperation(floorSettings.tilemap.CompressBounds);
+        return expandedPositions.Select(position => (IOperation) new TileAssignment
+            {
+                tile = tileset.NextValue(),
+                position = position,
+                tilemap = floorSettings.tilemap
+            }
+        );
     }
 
     private IEnumerable<Vector2Int> GetScaled(Vector2Int cell)
     {
-        for (int y = 0; y < layoutScale.y; y++)
+        for (int y = 0; y < layoutScale; y++)
         {
-            for (int x = 0; x < layoutScale.x; x++)
+            for (int x = 0; x < layoutScale; x++)
             {
                 yield return cell * layoutScale + new Vector2Int(x, y);
             }
